574d53d34eb76046f1e13d7bffcf1218c17ea39d
Dineo <dineo.maleka@go4ir.com>
2022-02-22 12:13:32 +0200

    Initial commit

--- ProjectTemplate2022/Assets/DOM_Scripts/VRLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class VRLoader : MonoBehaviour
-{
-    public static string modeVR = "off";
-    public static string mode2D = "off";
-}
--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Definition_Of_Momentum
-This is a project about momentum.
-
-Now, its time to share the project!
-
-Project is available for sharing.
-Testing file sharing 2
+ 
